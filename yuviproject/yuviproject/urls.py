9d1e545bb5deada1769a84731984ff4c73102929
YuvarajVB <yuva6542060@gmail.com>
2024-10-31 21:16:07 +0530

    Success

---
+++ yuviproject/yuviproject/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from yuviapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('surfacearea/',views.surfacearea,name="surfacearea"),
+    path('',views.surfacearea,name="surfcaearea")
+]
